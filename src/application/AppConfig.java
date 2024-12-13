e8294ec95cd9dca8caaf7fa5a71666adbffc9916
Md Asif Islam <lionelasif@outlook.com>
2024-12-13 13:36:04 +0800

    update 1

--- src/application/AppConfig.java
+++ src/application/AppConfig.java
@@ -5,6 +5,6 @@ import java.util.Map;
 public class AppConfig {
     // Predefined admin credentials
     public static final Map<String, String> ADMIN_CREDENTIALS = Map.of(
-        "admin", "password123" // Example: Username: admin, Password: password123
+        "admin", "1234" // Example: Username: admin, Password: password123
     );
 }
